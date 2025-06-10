87082b9488bae486a90644ded3a33e6e319bbd42
MD Sohidul Islam Ananto <anontom90@gmail.com>
2025-06-10 15:01:09 +0600

    user related stafs

---
+++ src/app/modules/user/user_controller.py
@@ -0,0 +1,30 @@
+from src.app.modules.user import user_service
+from src.utils.send_res import send_response
+from src.constants.http_status_code import HTTP_STATUS
+from fastapi import Request
+
+async def register_user(req: Request):
+
+    body = await req.json();
+
+    result = await user_service.register_user(body)
+
+    return send_response(
+        True,
+        "User created Successfull!",
+        result,
+        HTTP_STATUS.OK
+    )
+
+async def get_user(req: Request):
+
+    print(req)
+
+    result = await user_service.register_user()
+
+    return send_response(
+        True,
+        "Get user Successfull!",
+        result,
+        HTTP_STATUS.OK
+    )
--- src/app/modules/user/user_model.py
+++ src/app/modules/user/user_model.py
@@ -1,7 +1,8 @@
 from sqlmodel import SQLModel, Field
 from typing import Optional
+from uuid import uuid4
 
 class User(SQLModel, table=True):
-    id: Optional[int] = Field( default=None, primary_key=True )
+    id: Optional[int] = Field(default_factory=lambda: uuid4().int, primary_key=True, index=True)
     name: str
     email: str
