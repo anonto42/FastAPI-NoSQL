f06c7e19c1a0ed39a9edb6ec0423ce8b3f0475cc
MD Sohidul Islam Ananto <anontom90@gmail.com>
2025-06-11 15:13:29 +0600

    need work to create the db

--- server/app/modules/user/user_model.py
+++ server/app/modules/user/user_model.py
@@ -1,8 +1,36 @@
-from sqlmodel import SQLModel, Field
+from server.db.db_connection import db
 from typing import Optional
 from uuid import uuid4
 
-class User(SQLModel, table=True):
-    id: Optional[int] = Field(default_factory=lambda: uuid4().int, primary_key=True, index=True)
-    name: str
-    email: str
+class user_model:
+    def __init__(self, name: str, email: str, hashed_password: str, user_id: Optional[str] = None):
+        self.name = name
+        self.email = email
+        self.hashed_password = hashed_password
+        self.id = user_id if user_id else str(uuid4())
+
+    def save(self):
+        # Ensure db is initialized
+        if db is None:
+            raise Exception("Database connection is not established.")
+        
+        user_dict = {
+            "id": self.id,
+            "name": self.name,
+            "email": self.email,
+            "password": self.hashed_password
+        }
+        
+        result = db.users.insert_one(user_dict)
+        return result.inserted_id 
+
+    @classmethod
+    def find_by_email(cls, email: str):
+        # Ensure db is initialized
+        if db is None:
+            raise Exception("Database connection is not established.")
+            
+        user_data = db.users.find_one({"email": email})
+        if user_data:
+            return cls(**user_data)
+        return None
--- server/app/modules/user/user_service.py
+++ server/app/modules/user/user_service.py
@@ -1,9 +1,30 @@
-from .user_schema import user
-from .user_model import User as user_model
+from .user_schema import User
+from .user_model import user_model
 from ....db.db_connection import db
+from server.utils import crypto_context
+from server.constants.http_status_code import HTTP_STATUS
+from server.error.exceptions_error import APIError
 
 async def register_user(
-    req: user
+    payload: User
 ):
+    existing_user = user_model.find_by_email(payload.email)
+    if existing_user:
+        raise APIError(status_code=HTTP_STATUS.Not_Found, message="Email is already in use")
     
-    return dict()
+    hash_password = crypto_context.hash_password(payload.password)
+
+    try:
+        new_user = user_model(
+            name=payload.name,
+            email=payload.email,
+            hashed_password=hash_password
+        )
+
+        user = new_user.save()
+        
+        return dict(user)
+    
+    except ValueError as e :
+        print(ValueError)
+        raise  APIError(status_code=HTTP_STATUS.Internal_Server_Error, message=str(e))
