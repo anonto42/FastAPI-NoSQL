8a33a362dfc65ef4215188cf037269e167ba74a5
MD Sohidul Islam Ananto <anontom90@gmail.com>
2025-06-13 11:29:21 +0600

    facing problem on connect to the docker mongo image

--- server/config/env.py
+++ server/config/env.py
@@ -1,10 +1,9 @@
-import os
 from dotenv import load_dotenv
+import os
 
-load_dotenv()
+load_dotenv(dotenv_path="./.env")
 
 MONGO_URI = os.getenv("MONGO_URI")
-DATABASE_NAME = os.getenv("DATABASE_NAME")
 CRYPTO_SECRETE = os.getenv("CRYPTO_SECRETE")
 SENDER_PASSWORD = os.getenv("SENDER_PASSWORD")
 SENDER_EMAIL = os.getenv("SENDER_EMAIL")
--- server/db/db_connection.py
+++ server/db/db_connection.py
@@ -1,10 +1,11 @@
 from pymongo import MongoClient
-from server.config import env
+from ..config.env import DATABASE_NAME, MONGODB_ADMINPASSWORD, MONGODB_ADMINUSERNAME, MONGO_URI, HOST_IP
 from ..error.exceptions_error import APIError
 from ..constants.http_status_code import HTTP_STATUS
 from colorama import init, Fore, Back, Style
 
 init(autoreset=True)
+
 
 client = None
 db = None
@@ -14,11 +15,22 @@ def init_db():
     global client, db
     try:
         # Connect to MongoDB using the URI from the environment
-        client = MongoClient(env.MONGO_URI)
-        db = client[env.DATABASE_NAME]
+        # mongo_uri = "mongodb://localhost:27017/"
+        db_name = DATABASE_NAME
+        # mongo_uri = f"mongodb://{HOST_IP}:27017"
+        # mongo_uri = "mongodb://toor:root@mongo:27017"
+        # mongo_uri = f"mongodb://{MONGODB_ADMINUSERNAME}:{MONGODB_ADMINPASSWORD}@mongo:27017"
+        mongo_uri = "mongodb+srv://anonto:anontom90@cluster0.1zoujuq.mongodb.net"
+        
+        
+        # client = MongoClient(env.MONGO_URI)
+        client = MongoClient(mongo_uri)
+        
+        db = client[db_name]
+        
         print(Fore.MAGENTA + Back.GREEN + Style.BRIGHT + "DB connected successfully!")
-        print(Fore.BLUE + Back.YELLOW + Style.BRIGHT + "Database host is : ", Fore.GREEN + env.MONGO_URI)
-        print(Fore.LIGHTBLUE_EX + Back.CYAN + Style.BRIGHT + "Database name is : ", Fore.BLUE + env.DATABASE_NAME)
+        print(Fore.BLUE + Back.YELLOW + Style.BRIGHT + "Database host is : ", Fore.GREEN + client.HOST)
+        print(Fore.LIGHTBLUE_EX + Back.CYAN + Style.BRIGHT + "Database name is : ", Fore.BLUE + db_name)
         
         client.server_info()  # This will send an error if the connection fails
 
